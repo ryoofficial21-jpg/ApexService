e7717fc645df63bd7217104b53190f1a685be3e4
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-22 21:38:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: process.env.TOKEN,
+
+    // Bot Application ID (Developer Portal > General Information > Application ID)
+    clientId: '1518596273106849913',
+
+    // Discord Server ID
+    guildId: '1518593843572772904'
+};
